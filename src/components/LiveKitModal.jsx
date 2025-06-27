62ead052581c1203019187b1f814a4f22bf48931
greenduzt <chamara_sachintha@yahoo.com>
2025-06-27 11:33:20 +1000

    Livekitmodal update 2

--- src/components/LiveKitModal.jsx
+++ src/components/LiveKitModal.jsx
@@ -8,7 +8,6 @@ const LiveKitModal = ({ setShowSupport }) => {
   const [name, setName] = useState("");
   const [token, setToken] = useState(null);
 
-  // Function to fetch the token from the server
   const getToken = useCallback(async (userName) => {
   try {
 
